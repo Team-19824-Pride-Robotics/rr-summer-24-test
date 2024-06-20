84ef9d5448f0e62157f2c8780a68646a3a49acb7
Matt Owen <matthew_owen@lusherschool.org>
2024-06-19 19:49:33 -0500

    Update MeepMeepTesting2.java

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting2.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting2.java
@@ -16,20 +16,19 @@ public class MeepMeepTesting2 {
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-36, 58, Math.toRadians(270)))
-                        //.setTangent(Math.toRadians(180))
+
                         .lineToY(15)
-                        //.splineTo(new Vector2d(0, 29), Math.toRadians(90))
                         .splineTo(new Vector2d(-60, 10), Math.toRadians(180))
-                        //.setTangent(0)
-                        .lineToX(20)
-                        .splineToConstantHeading(new Vector2d(50, 40), Math.toRadians(0))
-                        .setTangent(Math.toRadians(270))
-                        .splineToConstantHeading(new Vector2d(40, 10), Math.toRadians(0))
-                        .lineToX(-60)
-                        .lineToX(20)
-                        .splineToConstantHeading(new Vector2d(50, 40), Math.toRadians(0))
-                        .setTangent(Math.toRadians(270))
-                        .lineToY(10)
+                        .lineToX(50)
+                        .strafeToConstantHeading(new Vector2d(50,38))
+                        .strafeToConstantHeading(new Vector2d(50,10))
+                        .setTangent(0)
+                        .lineToXConstantHeading(-60)
+                        .lineToX(50)
+                        .strafeToConstantHeading(new Vector2d(50,38))
+                        .setTangent(Math.toRadians(90))
+                        .lineToYConstantHeading(58)
+
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
