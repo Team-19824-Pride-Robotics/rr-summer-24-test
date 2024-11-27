a27c0801a874073620a08cea8222e6bdbf9abfef
Matt Owen <matthew_owen@lusherschool.org>
2024-11-27 13:22:24 -0600

    Update MeepMeepTesting.java

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,8 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -14,16 +16,57 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
-                .build());
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-62, -8, 0))
+                    //segment 1 - parallel with raise lift and arm at score height
+                    .lineToX(-36)
+                    //segment 2 - parallel with lift to pickup position
+                    .lineToX(-44)
+                    .turn(Math.toRadians(-180))
+                    .setTangent(0)
+                    .splineToConstantHeading(new Vector2d(-55, -45), Math.toRadians(180))
+                    //segment 3 - slowly!
+                    .lineToX(-62, new TranslationalVelConstraint(5))
+                    //segment 4 - back off the wall and turn
+                    .lineToX(-50)
+                    .turn(Math.toRadians(-180))
+                    //segment 5 - parallel with raise lift and arm at score height
+                    .splineToConstantHeading(new Vector2d(-36, -12), Math.toRadians(0))
+                //segment 2 - parallel with lift to pickup position
+                .lineToX(-44)
+                .turn(Math.toRadians(-180))
+                .setTangent(0)
+                .splineToConstantHeading(new Vector2d(-55, -45), Math.toRadians(180))
+                //segment 3 - slowly!
+                .lineToX(-62, new TranslationalVelConstraint(5))
+                //segment 4 - back off the wall and turn
+                .lineToX(-50)
+                .turn(Math.toRadians(-180))
+                //segment 5 - parallel with raise lift and arm at score height
+                .splineToConstantHeading(new Vector2d(-36, -16), Math.toRadians(0))
+                //segment 2 - parallel with lift to pickup position
+                .lineToX(-44)
+                .turn(Math.toRadians(-180))
+                .setTangent(0)
+                .splineToConstantHeading(new Vector2d(-55, -45), Math.toRadians(180))
+                //segment 3 - slowly!
+                .lineToX(-62, new TranslationalVelConstraint(5))
+
+
+
+
+    //                .strafeTo(new Vector2d(-36, -16))
+    //                .splineToConstantHeading(new Vector2d(0, -40), Math.toRadians(0))
+    //                .turn(Math.toRadians(180))
+    //                .splineToConstantHeading(new Vector2d(-55, -45), Math.toRadians(180))
+    //                .splineToConstantHeading(new Vector2d(0, -45), Math.toRadians(180))
+
+    //                .lineToY(30)
+    //                .turn(Math.toRadians(90))
+    //                .lineToX(0)
+    //                .turn(Math.toRadians(90))
+    //                .lineToY(0)
+    //                .turn(Math.toRadians(90))
+                    .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
                 .setDarkMode(true)
