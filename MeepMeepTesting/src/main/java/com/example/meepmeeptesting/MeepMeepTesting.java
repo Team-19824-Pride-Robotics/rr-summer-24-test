f5d2931f4e1e70caec76e8b74531bf5eaaba3d72
Matt Owen <matthew_owen@lusherschool.org>
2024-11-27 17:19:40 -0600

    Update MeepMeepTesting.java

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -17,36 +17,68 @@ public class MeepMeepTesting {
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-62, -8, 0))
-                    //segment 1 - parallel with raise lift and arm at score height
+                    //segment 1 - drives up to the sub and scores the preload
+                // parallel with lift to score height
                     .lineToX(-36)
-                    //segment 2 - parallel with lift to pickup position
-                    .lineToX(-50)
 
+                    //segment 2 - backs off the sub and strafes right to clear it
+                // parallel with lift to pickup position
+                    .lineToX(-40)
+                    .strafeTo(new Vector2d(-40, -35))
+
+                //segment 3 - moves on a diagonal to get behind the sample
+                    .setTangent(135)
+                    .lineToX(-12)
+
+                //segment 4 - spline path with a 180 built in, gets in position to push
                     .setTangent(0)
-                   // .splineTo(new Vector2d(-55, -45), Math.toRadians(180))
-                    .splineToLinearHeading(new Pose2d(-55, -45, Math.toRadians(180)), Math.toRadians(180))
+                    .splineToLinearHeading(new Pose2d(-10, -45, Math.toRadians(180)), Math.toRadians(180))
 
+                //segment 5 - push two samples into the zone
+                    .lineToX(-55)
+                    .lineToX(-12)
+                    .strafeTo(new Vector2d(-12, -55))
+                    .setTangent(135)
+                    .lineToX(-55)
 
-                    //.turn(Math.toRadians(-180))
-                    //segment 3 - slowly!
-                    .lineToX(-62, new TranslationalVelConstraint(5))
-                    //segment 4 - back off the wall and turn
+                //segment 6 - slowly! to pick up the specimen
+                    //.setTangent(135)
+                    .lineToX(-62, new TranslationalVelConstraint(10))
+
+                //segment 7 - spline path back to the sub with a 180
+                //parallel with lift to scoring position
                     .lineToX(-50)
                     .splineToLinearHeading(new Pose2d(-36, -12, Math.toRadians(0)), Math.toRadians(0))
 
-                .lineToX(-50)
+               //segment 8 - spline path back to the zone with a 180
+                // parallel with lift to pickup position
+                    .lineToX(-45)
+                    .setTangent(0)
+                    .splineToLinearHeading(new Pose2d(-55, -45, Math.toRadians(180)), Math.toRadians(180))
 
+                //segment 6 - slowly! to pick up the specimen
+                //.setTangent(135)
+                .lineToX(-62, new TranslationalVelConstraint(10))
+
+                //segment 7 - spline path back to the sub with a 180
+                //parallel with lift to scoring position
+                .lineToX(-50)
+                .splineToLinearHeading(new Pose2d(-36, -16, Math.toRadians(0)), Math.toRadians(0))
+
+                //segment 8 - spline path back to the zone with a 180
+                // parallel with lift to pickup position
+                .lineToX(-45)
                 .setTangent(0)
-                // .splineTo(new Vector2d(-55, -45), Math.toRadians(180))
                 .splineToLinearHeading(new Pose2d(-55, -45, Math.toRadians(180)), Math.toRadians(180))
-                //segment 3 - slowly!
-                .lineToX(-62, new TranslationalVelConstraint(5))
-                //segment 4 - back off the wall and turn
+
+                //segment 6 - slowly! to pick up the specimen
+                //.setTangent(135)
+                .lineToX(-62, new TranslationalVelConstraint(10))
+
+                //segment 7 - spline path back to the sub with a 180
+                //parallel with lift to scoring position
                 .lineToX(-50)
-                .splineToLinearHeading(new Pose2d(-36, -12, Math.toRadians(0)), Math.toRadians(0))
-
-
-
+                .splineToLinearHeading(new Pose2d(-36, -20, Math.toRadians(0)), Math.toRadians(0))
 
 
 //                    .turn(Math.toRadians(-180))
